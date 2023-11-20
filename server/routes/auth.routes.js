14a1af5756fe6bf37df48b22961f306e618a611c
hopeintheworld <jyissac9@gmail.com>
2023-11-20 19:51:03 +0900

    20231120 FEAT: auth/signup controller 추가 및 swagger에 추가

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,43 @@
+const controller = require("../controllers/auth.controller")
+
+/** 
+ * @swagger
+ * /api/signup:
+ *  post:
+ *    tags:
+ *      - Auth
+ *    name: signup
+ *    summary: register new user
+ *    description: required kakao_email, user_nickname, and gender. kakao_email and user_nickname must be a String, gender must be a String.
+ *    produces:
+ *      - application/json
+ *    consumes:
+ *      - application/json
+ *    parameters:
+ *      - name: body
+ *        in: body
+ *        schema:
+ *          type: object
+ *          properties:
+ *            kakao_email:
+ *              type: string
+ *            name:
+ *              type: string
+ *            gender:
+ *              type: boolean
+ *        required:
+ *          - kakao_email
+ *          - name
+ *          - gender
+ *    responses:
+ *      '200':
+ *        description: Success to register new user
+ *      '500':
+ *        description: Failed to register new user
+ * 
+*/
+
+
+module.exports = function(BASE_URI, app) {
+    app.post(BASE_URI + "signup", controller.signup)
+}
