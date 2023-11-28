72a98397feaaef56a9dedbccb0cb84a49de67815
YSBunny <yun0836@naver.com>
2023-11-28 12:42:10 +0900

    로그인 했을 때만 마이페이지 진입 가능

--- client/src/views/LandingPages/MyPage/MyInformation.vue
+++ client/src/views/LandingPages/MyPage/MyInformation.vue
@@ -22,6 +22,18 @@ onMounted(() => {
 import { useAuthStore } from "../../../stores/index.js"; // 실제 경로로 대체
 
 export default {
+  beforeRouteEnter(to, from, next) {
+    const authStore = useAuthStore();
+
+    // 로그인 여부 확인
+    if (!authStore.isLoggedIn) {
+      // 로그아웃 상태일 때 이전 페이지로 리디렉션
+      alert("로그인 후 이용해주세요.");
+    } else {
+      // 로그인 상태일 때 계속 페이지 진입
+      next();
+    }
+  },
   computed: {
     user() {
       const authStore = useAuthStore();
