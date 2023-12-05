f07fb1e3edf80315556d0462728d2c0019ec2915
YSBunny <yun0836@naver.com>
2023-12-05 20:40:08 +0900

    추천 페이지 버튼 모양 수정

--- client/src/examples/kakaomap/WorkList.vue
+++ client/src/examples/kakaomap/WorkList.vue
@@ -11,40 +11,39 @@
       </ul>
     </div>
   </template>
-  
-    
-    <script>
-    export default {
-      data() {
-        return {
-          touristSpots: [
-            { id: 1, name: "서울 탑골공원", lat: 37.578548, lng: 126.981799 },
-            { id: 2, name: "부산 해운대", lat: 35.158698, lng: 129.160199 },
-            // 추가 관광지 데이터
-          ],
-        };
+
+<script>
+  export default {
+    data() {
+      return {
+        touristSpots: [
+          { id: 1, name: "서울 탑골공원", lat: 37.578548, lng: 126.981799 },
+          { id: 2, name: "부산 해운대", lat: 35.158698, lng: 129.160199 },
+          // 추가 관광지 데이터
+        ],
+      };
+    },
+    methods: {
+      handleClick(spot) {
+        // 관광지 클릭 이벤트 발생
+        this.$emit("touristSpotClick", spot);
       },
-      methods: {
-        handleClick(spot) {
-          // 관광지 클릭 이벤트 발생
-          this.$emit("touristSpotClick", spot);
-        },
-      },
-    };
-    </script>
-  
-  <style scoped>
-  /* scoped를 사용하여 컴포넌트 내에서만 스타일이 적용되도록 설정 */
-  div {
-    border-radius: 10px;
-  }
-  
-  li {
-    cursor: pointer;
-    transition: background-color 0.3s ease;
-  }
-  
-  li:hover {
-    background-color: #dcdcdc;
-  }
-  </style>
+    },
+  };
+</script>
+
+<style scoped>
+/* scoped를 사용하여 컴포넌트 내에서만 스타일이 적용되도록 설정 */
+div {
+  border-radius: 10px;
+}
+
+li {
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+li:hover {
+  background-color: #dcdcdc;
+}
+</style>
