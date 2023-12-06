f75d65b48a1e8f6c3f746de87d7c00ac71963f9c
이도희 <leedohee@idohuiui-MacBookAir.local>
2023-12-06 15:04:13 +0900

    관광지 마커생기고사라짐

--- client/src/examples/kakaomap/KakaoMap.vue
+++ client/src/examples/kakaomap/KakaoMap.vue
@@ -1,177 +1,135 @@
 <template>
-    <div>
-      <!-- 사이드바 -->
-      <div class=" sidebar sidebar1 active text-center" style="background-color: white; box-shadow: 5px 0 10px rgba(0, 0, 0, 0.1); transform: translateX(0%);">
-        
-        <div class="row g-1 text-center">
-
-        <div class="col text-center " >
+  <div>
+    <!-- 사이드바 -->
+    <div class="sidebar sidebar1 active text-center" style="background-color: white; box-shadow: 5px 0 10px rgba(0, 0, 0, 0.1); transform: translateX(0%);">
+      <div class="row g-1 text-center">
+        <div class="col text-center ">
           <img src="@/assets/img/travel.png" style="width: 40px; height: 40px;">
           <p style="font-size: 13px;">관광지</p>
         </div>
-
-        </div>
-        
-        <div class="menu-separator">
-          
-        </div>
-
-        <div >
-        <!-- 사이드바1리스트 -->
-          <div  style="height: 450px; border-bottom: 1px solid #ccc;">
-            <TravelList @button-click="handleButtonClick" />
-          </div>
-
-
-
-
-        </div>
-        
       </div>
-      <!-- 사이드바2 -->
-      <div class=" sidebar sidebar2 active text-center" style="background-color: rgb(245, 245, 245); box-shadow: 5px 0 10px rgba(0, 0, 0, 0.1); transform: translateX(0%);" :class="{ active: sidebar2Visible }">
-        
-        <div class="row g-1 text-center">
-
-        <div class="col text-center " >
-            <img src="@/assets/img/study.png" style="width: 40px; height: 40px;">
-          <p  style="font-size: 13px;">업무공간</p>
+      <div class="menu-separator"></div>
+      <div>
+        <!-- 사이드바1리스트 -->
+        <div style="height: 450px; border-bottom: 1px solid #ccc;">
+          <TravelList @button-click="handleButtonClick" @tourist-spot-click="handleTravelSpotClick" />
         </div>
+      </div>
+    </div>
 
+    <!-- 사이드바2 -->
+    <div class="sidebar sidebar2 active text-center" style="background-color: rgb(245, 245, 245); box-shadow: 5px 0 10px rgba(0, 0, 0, 0.1); transform: translateX(0%);" :class="{ active: sidebar2Visible }">
+      <div class="row g-1 text-center">
+        <div class="col text-center ">
+          <img src="@/assets/img/study.png" style="width: 40px; height: 40px;">
+          <p style="font-size: 13px;">업무공간</p>
         </div>
-        
-        <div class="menu-separator"></div>
-
-        <div >
-          <!-- 사이드바2리스트 -->
+      </div>
+      <div class="menu-separator"></div>
+      <div>
+        <!-- 사이드바2리스트 -->
         <div style="height: 450px; border-bottom: 1px solid #ccc;">
           <WorkList @touristSpotClick="handleTouristSpotClick" />
         </div>
-        </div>
-        <div style="margin-top:5px">
-          <Button class="styled-button">업무공간저장</Button>
-        </div>
-        
       </div>
-  
-      <!-- 지도 -->
-      <div id="map"></div>
+      <div style="margin-top:5px">
+        <Button class="styled-button">업무공간저장</Button>
+      </div>
     </div>
-  </template>
-  <script>
-  import { toRaw } from "vue";
-  import TravelList from "./TravelList.vue";
-  import WorkList from "./WorkList.vue";
-  import axios from "axios";
-  
-  export default {
-    components: {
+
+    <!-- 지도 -->
+    <div id="map"></div>
+  </div>
+</template>
+
+<script>
+import { toRaw } from "vue";
+import TravelList from "./TravelList.vue";
+import WorkList from "./WorkList.vue";
+import axios from "axios";
+
+export default {
+  components: {
     TravelList,
     WorkList,
   },
 
+  name: "KakaoMap",
 
-    name: "KakaoMap",
-    
-    data() {
-      return {
-        markers: [],
-        infowindow: null,
-        sidebar2Visible: false,
-      };
-    },
-    mounted() {
-      if (window.kakao && window.kakao.maps) {
-        this.initMap();
+  data() {
+    return {
+      clickedMarker: null,
+      markers: [],
+      infowindow: null,
+      sidebar2Visible: false,
+    };
+  },
+
+  mounted() {
+    if (window.kakao && window.kakao.maps) {
+      this.initMap();
+    } else {
+      const script = document.createElement("script");
+      script.onload = () => kakao.maps.load(this.initMap);
+      script.src = "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=915cffed372954b7b44804ed422b9cf0";
+      document.head.appendChild(script);
+    }
+  },
+
+  methods: {
+    handleTravelSpotClick(coordinates) {
+      const { x, y, selectedSpots } = coordinates;
+      const markerPosition = new kakao.maps.LatLng(x, y);
+
+      const marginOfError = 0.00001;
+
+      const existingMarkerIndex = this.markers.findIndex(marker => {
+        const markerPosition = marker.getPosition();
+        const markerY = markerPosition.getLng();
+        const markerX = markerPosition.getLat();
+
+        return Math.abs(markerX - x) < marginOfError && Math.abs(markerY - y) < marginOfError;
+      });
+
+      if (existingMarkerIndex !== -1) {
+        const existingMarker = this.markers[existingMarkerIndex];
+        existingMarker.setMap(null);
+        this.markers.splice(existingMarkerIndex, 1);
       } else {
-        const script = document.createElement("script");
-        /* global kakao */
-        script.onload = () => kakao.maps.load(this.initMap);
-        script.src =
-          "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=915cffed372954b7b44804ed422b9cf0";
-        document.head.appendChild(script);
+        console.log('Creating new marker at:', x, y);
+        const marker = new kakao.maps.Marker({
+          position: markerPosition,
+          map: toRaw(this.map),
+        });
+
+        kakao.maps.event.addListener(marker, 'click', () => {
+          this.displayInfoWindow(markerPosition);
+        });
+
+        this.markers.push(marker);
       }
     },
-    methods: {
-      //하위컴포넌트에서 클릭시 사이드바토글관리
-      handleButtonClick() {
-        // 클릭 시 사이드바2의 상태를 토글
-      this.sidebar2Visible = !this.sidebar2Visible;
 
-      // 애니메이션 효과를 추가하려면 CSS 클래스를 추가/제거하면 됩니다.
-      // 여기서는 간단히 left 값을 변경하여 애니메이션을 효과를 줬습니다.
-      if (!this.sidebar2Visible) {
-        document.querySelector('.sidebar2').style.left = '250px';
-      } 
-      },
-      initMap() {
-        const container = document.getElementById("map");
-        const options = {
-          center: new kakao.maps.LatLng(35.458081, 126.10936),
-          level: 13,
-        };
-  
-        //지도 객체를 등록합니다.
-        //지도 객체는 반응형 관리 대상이 아니므로 initMap에서 선언합니다.
-        this.map = new kakao.maps.Map(container, options);
-      },
-      changeSize(size) {
-        const container = document.getElementById("map");
-        container.style.width = `${size}px`;
-        container.style.height = `${size}px`;
-        toRaw(this.map).relayout();
-      },
-      displayMarker(markerPositions) {
-        if (this.markers.length > 0) {
-          this.markers.forEach((marker) => marker.setMap(null));
-        }
-  
-        const positions = markerPositions.map(
-          (position) => new kakao.maps.LatLng(...position)
-        );
-  
-        if (positions.length > 0) {
-          this.markers = positions.map(
-            (position) =>
-              new kakao.maps.Marker({
-                map: toRaw(this.map),
-                position,
-              })
-          );
-  
-          const bounds = positions.reduce(
-            (bounds, latlng) => bounds.extend(latlng),
-            new kakao.maps.LatLngBounds()
-          );
-  
-          toRaw(this.map).setBounds(bounds);
-        }
-      },
-      displayInfoWindow() {
-        if (this.infowindow && this.infowindow.getMap()) {
-          //이미 생성한 인포윈도우가 있기 때문에 지도 중심좌표를 인포윈도우 좌표로 이동시킨다.
-          toRaw(this.map).setCenter(this.infowindow.getPosition());
-          return;
-        }
-  
-        var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-          iwPosition = new kakao.maps.LatLng(33.450701, 126.570667), //인포윈도우 표시 위치입니다
-          iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
-  
-        this.infowindow = new kakao.maps.InfoWindow({
-          map: toRaw(this.map), // 인포윈도우가 표시될 지도
-          position: iwPosition,
-          content: iwContent,
-          removable: iwRemoveable,
-        });
-  
-        toRaw(this.map).setCenter(iwPosition);
-      },
-      
+    initMap() {
+      const container = document.getElementById("map");
+      const options = {
+        center: new kakao.maps.LatLng(35.458081, 126.10936),
+        level: 13,
+      };
 
+      this.map = new kakao.maps.Map(container, options);
+    },
+
+    changeSize(size) {
+      const container = document.getElementById("map");
+      container.style.width = `${size}px`;
+      container.style.height = `${size}px`;
+      toRaw(this.map).relayout();
+    },
   },
 };
-  </script>
+</script>
+
   
   <style scoped>
   #map {
